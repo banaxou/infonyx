b3a0aa531eb7758879ec643f9677c2816c9aa42a
ovax <181485458+banaxou@users.noreply.github.com>
2025-07-26 11:39:31 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,3 +1,7 @@
+sudo apt update
+sudo apt install python3-pip python3-venv -y
+python3 -m venv ifx
+source ifx/bin/activate
 pip install requests
 pip install holehe
 pip install fade
@@ -5,4 +9,5 @@ pip install ignorant
 pip install python-nmap
 pip install cowsay
 pip install phonenumbers
-python ./infonyx.py
+
+python3 ./infonyx.py
